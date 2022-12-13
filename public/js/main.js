3d80ecfd2a7bfaa3161884aeabc95659e452e17a
Phuc Nguyen <nguyenphuc2002ok@gmail.com>
2022-12-13 10:55:15 +0700

    day2

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@ const cardPantsEl = document.querySelector(".product-list-pants");
 const btnAddToCart = document.querySelector(".btn-add-to-cart");
 const searchInput = document.querySelector(".search-input");
 const searchFormInputEl = document.querySelector(".seach-input");
+    //helllllo
 
 let items = getDataFromLocalStorage();
 const simple = (initial) => {
